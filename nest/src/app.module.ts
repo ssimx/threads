431b76af1490aae358cf4f00d0fdf70c7b4b40e4
vespucchi <56511717+vespucchi@users.noreply.github.com>
2025-04-26 11:22:54 +0200

    ADD(modules): users & comments

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UsersModule, CommentsModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
